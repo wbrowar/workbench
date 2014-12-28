83f5ed58564bdaafce246823d9604fbdbaa7d900
Will Browar <will_browar@dixonschwbl.com>
2014-12-28 18:39:45 -0500

    General updates in JS and CSS includes

---
+++ _source/_js/head.js
@@ -0,0 +1,3 @@
+/*
+  Used as a container for Modernizr w/ HTML5 shiv and any other javascript needed in the head tag.
+*/
---
+++ _source/_js/main.js
@@ -0,0 +1,27 @@
+var jq = jQuery;
+//var windowWidth = 0, windowWidth = 0, availHeight = 0;
+
+// UTILITY FUNCTIONS
+/*
+function ga_track(category, action, label) {
+  ga("send", "event", category, action, label);
+}
+*/
+
+// CUSTOM FUNCTIONS
+
+
+// BROWSER EVENTS
+/*
+jq(window).resize(function() {
+  windowWidth = jq(window).width()
+  windowHeight = jq(window).height()
+  availHeight = window.screen.availHeight
+});
+*/
+jq(document).ready(function() {
+  // do something
+});
+jq(window).load(function() {
+  // do something
+});
