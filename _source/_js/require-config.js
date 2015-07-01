47612e90afc7074c532c776caacabdd21551ed50
Will Browar <will_browar@dixonschwbl.com>
2015-07-01 11:35:14 -0400

    Updated with page-specific requirements

--- _source/_js/require-config.js
+++ _source/_js/require-config.js
@@ -1,5 +1,5 @@
 requirejs.config({
-	baseUrl: './js',
+	baseUrl: requireThemePath+'js',
 	paths: {
 		'jquery': 'lib/jquery/jquery.min',
 		'modernizr': 'lib/modernizr-custom.min',
@@ -14,8 +14,38 @@ requirejs.config({
 	urlArgs: "v=" + ((requireDevMode) ? (new Date()).getTime() : requireVersion),
 });
 
+// Global functions and scripts
+loadCSS(requireThemePath+'css/all.css?v='+requireVersion);
+
 if (requireSection === 'home') {
 	requirejs(["home"]);
+}
+
+// Get page-specific modules
+function requirePageSpecificModule(moduleName) {
+	switch(moduleName) {
+		/*
+		// EXAMPLES
+		case 'idealSlider':
+			requirejs(['idealSlider'], function() {
+				var idealSlider = new IdealImageSlider.Slider({
+					selector: '.body_gallery_wrapper',
+					interval: 4000,
+					transitionDuration: 1000,
+					effect: 'fade',
+				});
+				idealSlider.start();
+			});
+			break;
+		case 'formValidator':
+			requirejs(['formValidator']);
+			break;
+		*/
+	}
+}
+
+for (var i = 0; i < requireModules.length; i++) {
+	requirePageSpecificModule(requireModules[i]);
 }
 
 requirejs(["modernizr"]);
