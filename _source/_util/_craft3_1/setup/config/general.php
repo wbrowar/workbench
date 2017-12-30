51252f49f37884d777a98037ab28f8d1a09375d4
Will Browar <will_browar@dixonschwbl.com>
2017-12-29 21:39:16 -0500

    fixed mistake

--- _source/_util/_craft3_1/setup/config/general.php
+++ _source/_util/_craft3_1/setup/config/general.php
@@ -1,21 +1,58 @@
 <?php
 /**
- * Yii Application Config
+ * General Configuration
  *
- * Edit this file at your own risk!
- *
- * The array returned by this file will get merged with
- * vendor/craftcms/cms/src/config/app/main.php and [web|console].php, when
- * Craft's bootstrap script is defining the configuration for the entire
- * application.
- *
- * You can define custom modules and system components, and even override the
- * built-in system components.
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
  */
 
 return [
-    'modules' => [
-        'my-module' => \modules\Module::class,
+    // Global settings
+    '*' => [
+
+        'cacheDuration' => false,
+        'cpTrigger' => 'cms',
+        'defaultSearchTermOptions' => array(
+            'subLeft' => true,
+            'subRight' => true,
+        ),
+        'enableCsrfProtection' => true,
+        'generateTransformsBeforePageLoad' => true,
+        'omitScriptNameInUrls' => true,
+        'securityKey' => getenv('CRAFTENV_SECURITY_KEY'),
+        'siteUrl' => getenv('CRAFTENV_SITE_URL'),
+        'usePathInfo' => true,
+        'custom' => [
+            'basePath' => getenv('CRAFTENV_BASE_PATH'),
+            'baseUrl' => getenv('CRAFTENV_BASE_URL'),
+            'craftEnv' => CRAFT_ENVIRONMENT,
+        ]
     ],
-    //'bootstrap' => ['my-module'],
+
+    // Live (production) environment
+    'live' => [
+        // Craft defined config settings
+        'allowAutoUpdates' => false,
+        'backupOnUpdate' => false,
+        'devMode' => false,
+        'enableTemplateCaching' => true,
+    ],
+
+    // Staging (pre-production) environment
+    'staging' => [
+        // Craft defined config settings
+        'allowAutoUpdates' => false,
+        'backupOnUpdate' => false,
+        'devMode' => false,
+        'enableTemplateCaching' => true,
+    ],
+
+    // Local (development) environment
+    'local' => [
+        // Craft defined config settings
+        'allowAutoUpdates' => true,
+        'backupOnUpdate' => true,
+        'devMode' => true,
+        'enableTemplateCaching' => false,
+    ],
 ];
