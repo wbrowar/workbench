9bc5c247e4e4e09d39f583b8ad81adf02ac763c5
Will Browar <wbrowar@gmail.com>
2019-04-29 11:17:51 -0400

    Converted constants to class properties

--- _source/_js/adminbar.js
+++ _source/_js/adminbar.js
@@ -5,17 +5,19 @@ import { log, error } from './global.js';
 
 export default class AdminBar {
     constructor(args = {}) {
+        this.container = args.container ? document.querySelector(args.container) : document.body;
+        this.initOnLoad = args.initOnLoad || true;
+        this.location = args.location || 'top'; // top, bottom
+        this.params = args.params || {};
+        this.uri = args.uri || window.location.pathname.substr(1) || '__home__';
+
         // Check to see if functions from `{{ getAdminBarAssets() }}` are on the page
         // Also, doesn't render in IE to avoid JS errors
         if (typeof window.adminBarInit === "function" && !(/MSIE \d|Trident.*rv:/.test(navigator.userAgent))) {
-            const container = args.container ? document.querySelector(args.container) : document.body;
-            const init = args.init || true;
-            const location = args.location || 'top'; // top, bottom
-            const uri = args.uri || window.location.pathname.substr(1) || '__home__';
 
             const data = {
-                params: {},
-                uri: uri, // Get URI and pass it through. Use `__home__` for your homepage.
+                params: this.params, // Params that can be passed into Admin Bar
+                uri: this.uri, // Get URI and pass it through. Use `__home__` for your homepage.
             };
 
             fetch('/actions/admin-bar/bar', {
@@ -30,17 +32,15 @@ export default class AdminBar {
                     div.innerHTML = response.content;
 
                     if (div.children.length > 0) {
-                        if (location === 'top') {
-                            container.prepend(div.children[0]);
+                        if (this.location === 'top') {
+                            this.container.prepend(div.children[0]);
                         } else {
-                            container.appendChild(div.children[0]);
+                            this.container.appendChild(div.children[0]);
                         }
 
-                        if (init) {
+                        if (this.initOnLoad) {
                             // Call adminBarInit() to add Javascript events via the {{ getAdminBarAssets({ uri: craft.app.request.url }) }} Twig tag
                             this.init();
-
-                            log('Embedded Admin Bar for URI', uri);
                         }
                     }
                 })
@@ -49,6 +49,8 @@ export default class AdminBar {
     }
     init() {
         window.adminBarInit();
+
+        log('Embedded Admin Bar for URI', this.uri);
     }
 }
 
