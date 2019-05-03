313081361057f81f290fa76618734ef25715aed3
Will Browar <wbrowar@gmail.com>
2019-05-03 12:57:08 -0400

    Added .load to adminbar.js

--- _source/_js/adminbar.js
+++ _source/_js/adminbar.js
@@ -6,28 +6,40 @@ import { log, error } from './global.js';
 export default class AdminBar {
     constructor(args = {}) {
         this.container = args.container ? document.querySelector(args.container) : document.body;
-        this.initOnLoad = args.initOnLoad || true;
+        this.loaded = false;
         this.location = args.location || 'top'; // top, bottom
         this.params = args.params || {};
         this.uri = args.uri || window.location.pathname.substr(1) || '__home__';
+        this.wait = args.wait || false;
 
         // Check to see if functions from `{{ getAdminBarAssets() }}` are on the page
-        // Also, doesn't render in IE to avoid JS errors
-        if (typeof window.adminBarInit === "function" && !(/MSIE \d|Trident.*rv:/.test(navigator.userAgent))) {
+        if (typeof window.adminBarInit === "function" && window.fetch && !this.wait) {
+            if (this.load()) {
+                // Call adminBarInit() to add Javascript events via the {{ getAdminBarAssets({ uri: craft.app.request.url }) }} Twig tag
+                this.init();
+            }
+        }
+    }
+    static init() {
+        window.adminBarInit();
 
-            const data = {
-                params: this.params, // Params that can be passed into Admin Bar
-                uri: this.uri, // Get URI and pass it through. Use `__home__` for your homepage.
-            };
+        log('Embedded Admin Bar for URI', this.uri);
+    }
+    load(cb = false) {
+        const data = {
+            params: this.params, // Params that can be passed into Admin Bar
+            uri: this.uri, // Get URI and pass it through. Use `__home__` for your homepage.
+        };
 
-            fetch('/actions/admin-bar/bar', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then(res => res.json())
-                .then((response) => {
+        fetch('/actions/admin-bar/bar', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then((response) => {
+                if (response.response === 'success') {
                     const div = document.createElement('div');
                     div.innerHTML = response.content;
 
@@ -38,19 +50,19 @@ export default class AdminBar {
                             this.container.appendChild(div.children[0]);
                         }
 
-                        if (this.initOnLoad) {
-                            // Call adminBarInit() to add Javascript events via the {{ getAdminBarAssets({ uri: craft.app.request.url }) }} Twig tag
-                            this.init();
-                        }
-                    }
-                })
-                .catch(errorMessage => error('Error:', errorMessage));
-        }
-    }
-    init() {
-        window.adminBarInit();
+                        this.loaded = true;
 
-        log('Embedded Admin Bar for URI', this.uri);
+                        if (typeof cb === 'function') {
+                            cb();
+                        }
+
+                        return true;
+                    }
+                }
+
+                return false;
+            })
+            .catch(errorMessage => error('Error:', errorMessage));
     }
 }
 
