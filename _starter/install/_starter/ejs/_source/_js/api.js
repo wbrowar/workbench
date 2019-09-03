2dcf571532f57ebdf107e93933c0306921361a4d
Will Browar <wbrowar@gmail.com>
2019-09-02 22:13:35 -0400

    Added api.js file

---
+++ _starter/install/_starter/ejs/_source/_js/api.js
@@ -0,0 +1,56 @@
+//  API
+//  📬 Interact with external apis
+
+import axios from 'axios';
+import { log } from './global.js';
+
+const defaultHeaders = {
+    'Content-Type': 'application/json;charset=UTF-8',
+};
+const defaultUrl = 'REPLACE_URL';
+
+export default class Api {
+    constructor(args = {}) {
+        this.headers = args.headers || defaultHeaders;
+        this.callback = args.callback;
+        this.data = args.data || null;
+        this.query = args.query || null;
+        this.status = 'idle';
+        this.type = args.type || 'graphql';
+        this.url = args.url || defaultUrl;
+        this.variables = args.variables || null;
+
+        if (this.query) {
+            this.request(this.query)
+        }
+    }
+
+    graphqlRequest(query, variables = null) {
+        if (this.status === 'idle') {
+            this.status = 'loading';
+            axios({
+                url: this.url,
+                method: 'post',
+                data: this.data || {
+                    query: query,
+                    variables: variables,
+                },
+                headers: this.headers,
+            }).then((result) => {
+                this.callback(result);
+                this.status = 'idle';
+            });
+        }
+    }
+
+    request(query, variables = null) {
+        switch (this.type) {
+            case 'graphql':
+                this.graphqlRequest(query, variables || null);
+                break;
+        }
+    }
+}
+
+// INIT FUNCTIONS
+log('API');
