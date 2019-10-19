e9ccd065c9eb699f5a81991439108a42d890eb02
Will Browar <wbrowar@gmail.com>
2019-10-19 11:04:59 -0400

    Updated vue-cli installation

--- _starter/install.js
+++ _starter/install.js
@@ -36,7 +36,7 @@ let ejsVars = {
 };
 
 // set other variables
-const starterProjects = ['craft3', 'craftplugin', 'html', 'vue'];
+const starterProjects = ['craft3', 'craftplugin', 'html'];
 let localConfig = false;
 
 // check if npm and composer are installed
@@ -237,15 +237,6 @@ async function run() {
             },
             validate: (answer) => {
                 return answer !== '';
-            },
-        },
-        {
-            type: 'confirm',
-            name: 'headless',
-            message: 'Will this be a headless CMS?',
-            default: false,
-            when: (answers) => {
-                return ['craft3'].includes(answers.projectType);
             },
         },
         {
