49b973723aae0b18af06d566500b9c9cbee176b8
Will Browar <wbrowar@gmail.com>
2020-06-08 14:30:56 -0400

    Added favicon meta info

--- _starter/install/nuxt/mv/layouts/default.vue
+++ _starter/install/nuxt/mv/layouts/default.vue
@@ -9,11 +9,17 @@
 </template>
 
 <script>
+import { faviconVueMeta } from 'JS/favicons.js';
 import wb from 'JS/automated/wb.js';
 
 export default {
   components: {
     DevBar: () => import('Components/dev_bar/DevBar.vue'),
+  },
+  head() {
+    return {
+      ...faviconVueMeta,
+    };
   },
   data() {
     return {
