8f884d4abd235401c3f3944da061286957b550f5
Will Browar <wbrowar@gmail.com>
2020-10-01 10:58:06 -0400

    Moved scrollBehavior to app directory

---
+++ _install/_scaffolding/nuxt/mv/app/router.scrollBehavior.js
@@ -0,0 +1,15 @@
+export default function (to, from, savedPosition) {
+  if (to.hash) {
+    const element = document.querySelector(to.hash);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  } else if (savedPosition) {
+    return savedPosition;
+  } else {
+    return { x: 0, y: 0 };
+  }
+}
